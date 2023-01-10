afd877ee4693c9e22f74266bff81c5106e3f51c4
Fernando B. Alves <fbragaalves97@gmail.com>
2023-01-10 18:57:13 -0300

    Notes store implementado

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNotesRequest;
+use App\Http\Requests\UpdateNotesRequest;
+use App\Models\Notes;
+use Illuminate\Http\Client\Request;
+
+class NotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        return response()->json(Notes::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return Notes
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNotesRequest  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreNotesRequest $request)
+    {
+        $note = new Notes();
+        $note->title = $request->title;
+        $note->description = $request->description;
+
+        $note->save();
+
+        return response()->json($note);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Notes  $notes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Notes $notes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Notes  $notes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Notes $notes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNotesRequest  $request
+     * @param  \App\Models\Notes  $notes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNotesRequest $request, Notes $notes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Notes  $notes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Notes $notes)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreNotesRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreNotesRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules() {
+        return [
+            'title'       => 'required|unique:notes|max:255',
+            'description' => 'required'
+        ];
+    }
+
+    protected function failedValidation( Validator $validator ) {
+        throw new HttpResponseException(
+            response()->json( [
+                'success' => false,
+                'message' => 'Validation errors',
+                'data'    => $validator->errors()
+            ] )->setStatusCode( 422 )
+        );
+    }
+
+    public function messages() {
+        return [
+            "title.required"       => "Title is required",
+            "title.unique"         => "Title most to be unique",
+            "title.max"            => "Title must be a maximum of 255 characters",
+            "description.required" => "Description is required"
+        ];
+    }
+
+}
